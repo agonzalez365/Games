94d4365d48f469a91b8be034504585e625032d34
Andrew Gonzalez <drewgonz974@gmail.com>
2022-05-28 17:45:51 -0700

    Expanded click event, not quite done. Redid HTML ids, and formatted document

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ window.onload = function () {
     let team1Pieces;
     let team2Pieces;
     //the elements will function as board state
+    //it will look like this
+    /*
+    y 
+    0)x 0 1 2 3 
+    1)x 0 1 2 3
+    2)x 0 1 2 3
+    3)x 0 1 2 3
+    */
 
     //buttons/elements
     const startBtn = $('#start');
@@ -11,40 +19,75 @@ window.onload = function () {
     const turnText = $('#currentTurn');
 
     //custom event listeners
-    $(document).on('changeTurn', function() {
+    $(document).on('changeTurn', function () {
         console.log('turn changed');
     })
 
     //board event listeners
-    $('.cell').on('click', function() {
-        console.log($(this));
+    //this is ugly but it seemed like the simplest solution
+    //this event is how the player interacts with the board, when you click on a cell,
+    //it will evaluate what moves (if any) can be done
+    $('.cell').on('click', function () {
+        //check if piece is present
+        //console.log($(this).attr('id'));
+        console.log($(this).attr('id').slice(1));
+        if ($(this).children().length > 0) {
+            //check what team the piece is on and assign team for later comparison
+            let piece = $(this).children().eq(0);
+            let team;
+            if (piece.hasClass('team1')) {
+                team = 1;
+            }
+            else {
+                team = 2
+            }
+
+            //if it is that pieces turn, proceed
+            if (turn === team) {
+                //determine the x and y coordinates from ids
+                let x = Number($(this).attr('id').slice(1));
+                let y = Number($(this).parent().attr('id'));
+                
+                //uncomment to check x
+                //console.log('x:', x);
+                //console.log('y:', y);
+
+
+                //check if king before checking if a movement is available
+                if (piece.hasClass('king')) {
+
+                }
+                else {
+
+                }
+            }
+        }
     })
 
-    $('.cell').on('mouseenter', function() {
-        console.log('hover')
+    $('.cell').on('mouseenter', function () {
+        //console.log('hover')
     })
 
-    $('.cell').on('mouseleave', function() {
-        
+    $('.cell').on('mouseleave', function () {
+
     })
 
     //button functionality
     startBtn.on('click', function () {
         startGame();
-        
     });
 
     //function to begin a game
     function startGame() {
         //populate the board
-        resetBoard();
+        //resetBoard();
         populateBoard();
         determineFirstTurn();
 
         //initialize state
         team1Pieces = 16;
         team2Pieces = 16;
-        
+
     }
 
     //populates the board with pieces at default starting positions
@@ -52,13 +95,13 @@ window.onload = function () {
         //populate team1 side
         let row = 0;
         //outer loops iterate through specific rows that will be populated
-        for(row; row <= 2; row++){
+        for (row; row <= 2; row++) {
             //select the current row
             let currentRow = board.children().eq(row);
 
             //determine the startPoint for cell population to begin from - basically determines whether to indent the row
             let startPoint;
-            if(row === 1){
+            if (row === 1) {
                 startPoint = 0;
             }
             else {
@@ -66,35 +109,36 @@ window.onload = function () {
             }
 
             //populates every other cell from starting point in current row
-            for(let i = startPoint; i < 8; i += 2){
+            for (let i = startPoint; i < 8; i += 2) {
                 currentRow.children().eq(i).append(`<piece class="team1"></piece>`);
-                
+
             }
         }
 
         //populate team2 side - same as team1 population, iterating over different rows
         row = 5;
-        for(row; row <= 7; row++){
+        for (row; row <= 7; row++) {
             let currentRow = board.children().eq(row);
-            
+
             let startPoint;
-            if(row === 6){
+            if (row === 6) {
                 startPoint = 1;
             }
             else {
                 startPoint = 0;
             }
 
-            for(let i = startPoint; i < 8; i += 2){
+            for (let i = startPoint; i < 8; i += 2) {
                 currentRow.children().eq(i).append(`<piece class="team2"></piece>`);
             }
         }
-        
+
     }
 
     //resets the board
     function resetBoard() {
         //instead of checking for pieces and removing them, this just empties the entire board of elements and appends back the default
+        //REDO, this removes events
         board.empty();
         board.append(`
         <div class="row">
@@ -180,17 +224,17 @@ window.onload = function () {
         `)
     }
 
-    function determineFirstTurn(){
+    function determineFirstTurn() {
         //return either 1 or 2, which will determine who goes first
         turn = Math.ceil(Math.random() * 2);
-        console.log(turn);
-        console.log(turnText.text());
-        if(turn === 1){
+        if (turn === 1) {
             turnText.text('Team 1');
         }
         else {
             turnText.text('Team 2');
         }
     }
+
+
 
 }
